2b3d1b84e7b7e76d56298d5feb11c1747dfad6a9
Bhoj Sao <bhojsao@ymail.com>
2019-10-06 14:14:24 +0530

    Added Properties file and Main class

---
+++ src/main/java/com/bhoj/springboot/AuthServiceApp.java
@@ -0,0 +1,17 @@
+package com.bhoj.springboot;
+
+/**
+ * @author Bhoj Sao
+ *
+ */
+public class AuthServiceApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
